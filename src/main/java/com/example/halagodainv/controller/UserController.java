47a373b5a83d1fa1d62f0134fc9b9ba661bac457
NamPink <namph@mor.com.vn>
2023-07-12 10:18:08 +0700

    fix api

--- src/main/java/com/example/halagodainv/controller/UserController.java
+++ src/main/java/com/example/halagodainv/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.halagodainv.controller;
 
 import com.example.halagodainv.config.filter.JwtToken;
+import com.example.halagodainv.model.UserEntity;
+import com.example.halagodainv.repository.UserRepository;
 import com.example.halagodainv.request.UserLogin;
 import com.example.halagodainv.response.BaseResponse;
 import com.example.halagodainv.response.UserResponse;
@@ -16,6 +18,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -23,6 +26,8 @@ public class UserController {
     Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserServiceConfig authConfig;
+    @Autowired
+    private UserRepository userRepository;
     @Autowired
     AuthenticationManager authenticationManager;
     @Autowired
@@ -33,9 +38,10 @@ public class UserController {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLogin.getEmail(), userLogin.getPassword()));
             UserDetails userDetails = authConfig.loadByEmail(userLogin.getEmail());
+            Optional<UserEntity> userEntity = userRepository.findByEmail(userDetails.getUsername());
             String token = jwtToken.generateToken(userDetails);
             String refreshToken = jwtToken.generateRefreshToken(userDetails);
-            return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>(HttpStatus.OK.value(), "login success", new UserResponse(token, refreshToken)));
+            return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>(HttpStatus.OK.value(), "login success", new UserResponse(userEntity.get().getUserName(), token, refreshToken)));
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
@@ -45,10 +51,11 @@ public class UserController {
     public ResponseEntity<BaseResponse<Object>> user(@RequestParam("refreshToken") String refreshToken) {
         try {
             String accessToken = jwtToken.getUserNameFromJWT(refreshToken);
-            UserDetails userDetails = authConfig.loadUserByUsername(accessToken);
+            UserDetails userDetails = authConfig.loadByEmail(accessToken);
+            Optional<UserEntity> userEntity = userRepository.findByEmail(userDetails.getUsername());
             String newToken = jwtToken.generateToken(userDetails);
             String refreshTokenNew = jwtToken.generateRefreshToken(userDetails);
-            return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>(HttpStatus.OK.value(), "login success", new UserResponse(newToken, refreshTokenNew)));
+            return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>(HttpStatus.OK.value(), "login success", new UserResponse(userEntity.get().getUserName(),newToken, refreshTokenNew)));
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
 
--- src/main/java/com/example/halagodainv/response/UserResponse.java
+++ src/main/java/com/example/halagodainv/response/UserResponse.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserResponse {
+    private String userName;
     private String token;
     private String refreshToken;
 }
