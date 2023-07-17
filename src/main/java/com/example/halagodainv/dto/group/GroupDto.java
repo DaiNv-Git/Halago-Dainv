5196346a3dec21c080753996790cc953664c29e0
NamPink <namph@mor.com.vn>
2023-07-17 18:16:26 +0700

    fix api campaign and news

--- src/main/java/com/example/halagodainv/dto/group/GroupDto.java
+++ src/main/java/com/example/halagodainv/dto/group/GroupDto.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.time.DateFormatUtils;
 
-import javax.persistence.Column;
 import java.util.Date;
 
 @Data
@@ -16,18 +15,18 @@ public class GroupDto {
     private String groupName;
     private String phone;
     private String link;
-    private String memTotal;
+    private String memberTotal;
     private String expense;
     private String created;
     private String industry;
     private int industryId;
 
-    public GroupDto(long id, String groupName, String phone, String link, String memTotal, String expense, Date created, String industry,int industryId) {
+    public GroupDto(long id, String groupName, String phone, String link, String memberTotal, String expense, Date created, String industry, int industryId) {
         this.id = id;
         this.groupName = groupName;
         this.phone = phone;
         this.link = link;
-        this.memTotal = memTotal;
+        this.memberTotal = memberTotal;
         this.expense = expense;
         this.created = DateFormatUtils.format(created, "dd-MM-yyyy");
         this.industry = industry;
