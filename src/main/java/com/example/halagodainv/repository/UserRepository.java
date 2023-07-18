5753f6d367c6d0e470ac684d9cd0dd5bb609fdf8
NamPink <namph@mor.com.vn>
2023-07-18 12:42:43 +0700

    fix api campaign and news

--- src/main/java/com/example/halagodainv/repository/UserRepository.java
+++ src/main/java/com/example/halagodainv/repository/UserRepository.java
@@ -23,10 +23,10 @@ public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     UserDto getUser(@Param("userId") int userId);
 
     @Query("select new com.example.halagodainv.dto.user.UserDto(u.id,u.email,u.userName,r.name) from UserEntity u left join RoleEntity r " +
-            "on r.idRole = u.role where u.userName like concat('%',:userName,'%') ")
+            "on r.idRole = u.role where ifnull(u.userName,'') like concat('%',:userName,'%') ")
     List<UserDto> getAll(@Param("userName") String userName, Pageable pageable);
     @Query("select count(u) from UserEntity u left join RoleEntity r " +
-            "on r.idRole = u.role where u.userName like concat('%',:userName,'%')")
+            "on r.idRole = u.role where ifnull(u.userName,'')  like concat('%',:userName,'%')")
     int  totalElementAll(@Param("userName") String userName);
 
 
