127e529727afec03c7835028d879a1fb4116f9c3
AnhPD <Ducanhpt1303@gmail.com>
2023-09-12 08:56:57 +0700

    update api

--- src/main/java/com/example/halagodainv/until/FileImageUtil.java
+++ src/main/java/com/example/halagodainv/until/FileImageUtil.java
@@ -6,22 +6,14 @@ import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.HtmlUtils;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
